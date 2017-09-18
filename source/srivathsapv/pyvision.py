6970361cf41c374df13a686b79c9b26d6a2d2d45
Srivathsa <srivathsa@Srivathsas-MacBook-Pro.local>
2017-09-18 15:10:09 -0400

    Add watershed sample code snippet

---
+++ source/srivathsapv/pyvision.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pylab
+import mahotas as mh
+dna = mh.imread('input.jpg')
+pylab.imshow(dna)
+pylab.show()
+
+T = mh.thresholding.otsu(dna)
+out_image = (dna > T)
+out_image = out_image.astype(np.uint8) * 255
+pylab.imshow(out_image)
+pylab.show()
+
+labeled, nr_objects = mh.label(out_image > T)
+print nr_objects
+
+pylab.imshow(labeled.astype(np.uint8) * 2555)
+pylab.jet()
+pylab.show()
