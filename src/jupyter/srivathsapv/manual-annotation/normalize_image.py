9e4f340fe5cc226258285b1cdf3ffd0f7e8e2666
Srivathsa <psrivat1@jhu.edu>
2017-11-04 15:58:26 -0400

    Change image normalization code to support varying dimensions. Add spectral cluster test code

--- src/jupyter/srivathsapv/manual-annotation/normalize_image.py
+++ src/jupyter/srivathsapv/manual-annotation/normalize_image.py
@@ -10,44 +10,28 @@ from ImageDrawer import ImageDrawer
 from skimage.filters import threshold_otsu, gaussian
 from skimage.morphology import opening
 
-img = tiff.imread('s3617_cutout.tif')
+tif_img = tiff.imread('subvolumes/cell_detection_0.tiff').astype(float)
+
+tiff_img = tif_img.astype(np.uint16)
+
 tif_img *= (255.0/ tif_img.max())
 
-img = np.ndarray((100,1000,1000,3))
+shape_z, shape_y, shape_x = tif_img.shape
+
+img = np.ndarray((shape_z, shape_y, shape_x, 3))
 img[:, :, :, 0] = tif_img
 img[:, :, :, 1] = tif_img
 img[:, :, :, 2] = tif_img
 
-with open('centroids.csv') as csv_file:
-    reader = csv.reader(csv_file)
-    centroids = list(reader)
+tiff.imsave('subvolumes/cell_detection_0.tiff', img.astype(np.uint8))
 
-subvolume_img = img[:, 300:400, 650:750, :]
-
-for z in range(0,100):
+for z in range(0,shape_z):
     print('{}-slice'.format(z))
-    for y in range(0,1000):
-        for x in range(0,1000):
+    for y in range(0,shape_y):
+        for x in range(0,shape_x):
             if img[z,y,x, 0] > 30:
                 img[z,y,x, :] = [255, 255, 255]
             else:
                 img[z,y,x, :] = [0, 0, 0]
 
-tiff.imsave('s3617_cutout_normalized.tif', img.astype(np.uint8))
-
-centroids = [[math.ceil(float(c[0].split('.')[1])), math.ceil(float(c[1].split('.')[1])), math.ceil(float(c[2])] for c in centroids]
-
-formatted_centroids = []
-
-for c in centroids:
-    cz = int(c[0])-1
-    cy = int(math.ceil(float(c[1]) * (1000/39.37)))
-    cx = int(math.ceil(float(c[2]) * (1000/39.37)))
-    formatted_centroids.append([cz, cy, cx])
-
-csv_file = open('centroids_formatted.csv', 'w')
-
-for c in formatted_centroids:
-    csv_file.write(','.join(map(str,c)) + '\n')
-
-csv_file.close()
+tiff.imsave('subvolumes-normalized/cell_detection_0.tiff', img.astype(np.uint8))
